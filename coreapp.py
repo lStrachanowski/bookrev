2693a8bfa41409552f5c2e6f939a4f4147ae9921
łukasz Strachanowski <lukasz.strachanowski@gmail.com>
2018-07-22 10:52:55 +0200

    routes added

--- coreapp.py
+++ coreapp.py
@@ -6,4 +6,22 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
+@app.route('/join')
+def join():
+    return render_template('join.html')
+
+@app.route('/search')
+def search():
+    return render_template('search.html')
+
+@app.route('/results')
+def results():
+    return render_template('results.html')
+
+@app.route('/book')
+def book():
+    return render_template('book.html')
